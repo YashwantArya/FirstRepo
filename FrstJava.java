907d72b16a348400c7c92fa3d6200def7bdf9f5c
YashAdmin <yarya50@gmail.com>
2022-01-24 23:19:29 +0530

    Third Commit

--- FrstJava.java
+++ FrstJava.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World Yash!"); 
+        System.out.println("Hello, World Yashwant Arya!"); 
     }
 }
